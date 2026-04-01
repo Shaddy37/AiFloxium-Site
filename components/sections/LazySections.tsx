89cce0a593762cd555cd0b846308b5aa9a455e5e
ShadabShams <boyshaddy107@gmail.com>
2026-04-01 12:08:38 +0500

    feat: remove founder section from homepage

--- components/sections/LazySections.tsx
+++ components/sections/LazySections.tsx
@@ -8,10 +8,6 @@ const Trust = dynamic(() => import("@/components/sections/HomeSections").then(mo
 const TechStack = dynamic(() => import("@/components/sections/HomeSections").then(mod => mod.TechStack), { ssr: false });
 const Founder = dynamic(() => import("@/components/sections/HomeSections").then(mod => mod.Founder), { ssr: false });
 const Pricing = dynamic(() => import("@/components/sections/HomeSections").then(mod => mod.Pricing), { ssr: false });
-const IntegrationsSection = dynamic(() => import("@/components/ui/integrations-section"), { ssr: false });
-const Contact2 = dynamic(() => import("@/components/ui/contact-2").then(mod => mod.Contact2), { ssr: false });
-const TestimonialsMinimal = dynamic(() => import("@/components/ui/minimal-testimonial").then(mod => mod.TestimonialsMinimal), { ssr: false });
-const ProjectFocus = dynamic(() => import("@/components/sections/ProjectFocus"), { ssr: false });
 
 export function LazySections() {
   return (
@@ -22,12 +18,6 @@ export function LazySections() {
       <TechStack />
       <Founder />
       <Pricing />
-      <ProjectFocus />
-      <IntegrationsSection />
-      <Contact2 />
-      <TestimonialsMinimal />
     </>
   );
 }
-
-export { TestimonialsMinimal };
